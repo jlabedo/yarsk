dbb46aae43f32b8f2845774cc0d815c22c85ea60
Jean de La Bédoyère <jlabedo@gmail.com>
2015-10-17 17:20:03 +0200

    Linting

--- conf/make-webpack-config.js
+++ conf/make-webpack-config.js
@@ -1,32 +1,32 @@
-var fs = require('fs');
-var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+var fs = require('fs')
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var path = require('path')
 
-function extractForProduction(loaders) {
-  return ExtractTextPlugin.extract('style', loaders.substr(loaders.indexOf('!')));
+function extractForProduction (loaders) {
+  return ExtractTextPlugin.extract('style', loaders.substr(loaders.indexOf('!')))
 }
 
-module.exports = function(options) {
-  options.lint = fs.existsSync(__dirname + '/../.eslintrc') && options.lint !== false;
+module.exports = function (options) {
+  options.lint = fs.existsSync(__dirname + '/../.eslintrc') && options.lint !== false
 
-  var localIdentName = options.production ? '[hash:base64]' : '[path]-[local]-[hash:base64:5]';
-  var cssLoaders = 'style!css?localIdentName=' + localIdentName + '!autoprefixer?browsers=last 2 versions';
-  var postcssLoaders = 'style!css?module&importLoaders=1&localIdentName=' + localIdentName + '!postcss';
-  var scssLoaders = cssLoaders + '!sass';
-  var sassLoaders = scssLoaders + '?indentedSyntax=sass';
-  var lessLoaders = cssLoaders + '!less';
+  var localIdentName = options.production ? '[hash:base64]' : '[path]-[local]-[hash:base64:5]'
+  var cssLoaders = 'style!css?localIdentName=' + localIdentName + '!autoprefixer?browsers=last 2 versions'
+  var postcssLoaders = 'style!css?module&importLoaders=1&localIdentName=' + localIdentName + '!postcss'
+  var scssLoaders = cssLoaders + '!sass'
+  var sassLoaders = scssLoaders + '?indentedSyntax=sass'
+  var lessLoaders = cssLoaders + '!less'
 
   if (options.production) {
-    cssLoaders = extractForProduction(cssLoaders);
-    postcssLoaders = extractForProduction(postcssLoaders);
-    sassLoaders = extractForProduction(sassLoaders);
-    scssLoaders = extractForProduction(scssLoaders);
-    lessLoaders = extractForProduction(lessLoaders);
+    cssLoaders = extractForProduction(cssLoaders)
+    postcssLoaders = extractForProduction(postcssLoaders)
+    sassLoaders = extractForProduction(sassLoaders)
+    scssLoaders = extractForProduction(scssLoaders)
+    lessLoaders = extractForProduction(lessLoaders)
   }
 
-  var jsLoaders = ['babel'];
+  var jsLoaders = ['babel']
 
   return {
     entry: './app/index.jsx',
@@ -35,72 +35,72 @@ module.exports = function(options) {
     output: {
       path: options.production ? './dist' : './build',
       publicPath: options.production ? '' : 'http://localhost:8080/',
-      filename: options.production ? 'app.[hash].js' : 'app.js',
+      filename: options.production ? 'app.[hash].js' : 'app.js'
     },
     module: {
       preLoaders: options.lint ? [
         {
           test: /\.jsx?$/,
           exclude: /node_modules/,
-          loader: 'eslint',
-        },
+          loader: 'eslint'
+        }
       ] : [],
       loaders: [
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          loaders: jsLoaders,
+          loaders: jsLoaders
         },
         {
           test: /\.jsx$/,
           exclude: /node_modules/,
-          loaders: options.production ? jsLoaders : ['react-hot'].concat(jsLoaders),
+          loaders: options.production ? jsLoaders : ['react-hot'].concat(jsLoaders)
         },
         {
           test: /\.css$/,
-          loader: postcssLoaders,
+          loader: postcssLoaders
         },
         {
           test: /\.sass$/,
-          loader: sassLoaders,
+          loader: sassLoaders
         },
         {
           test: /\.scss$/,
-          loader: scssLoaders,
+          loader: scssLoaders
         },
         {
           test: /\.less$/,
-          loader: lessLoaders,
+          loader: lessLoaders
         },
         {
           test: /\.png$/,
-          loader: "url?limit=100000&mimetype=image/png",
+          loader: 'url?limit=100000&mimetype=image/png'
         },
         {
           test: /\.svg$/,
-          loader: "url?limit=100000&mimetype=image/svg+xml",
+          loader: 'url?limit=100000&mimetype=image/svg+xml'
         },
         {
           test: /\.gif$/,
-          loader: "url?limit=100000&mimetype=image/gif",
+          loader: 'url?limit=100000&mimetype=image/gif'
         },
         {
           test: /\.jpg$/,
-          loader: "file",
+          loader: 'file'
         },
         {
           test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "url-loader?limit=10000&minetype=application/font-woff"
+          loader: 'url-loader?limit=10000&minetype=application/font-woff'
         },
         {
           test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "file-loader"
-        },
-      ],
+          loader: 'file-loader'
+        }
+      ]
     },
-    postcss: function() {
-      var relativePath = path.resolve(this.resource);
-      console.log(relativePath);
+    postcss: function () {
+      var relativePath = path.resolve(this.resource)
+      console.log(relativePath)
       return [
         require('postcss-nested'),
         require('postcss-import')({
@@ -109,40 +109,40 @@ module.exports = function(options) {
           // https://github.com/postcss/postcss-import
 
           onImport: function (files) {
-            files.forEach(this.addDependency);
+            files.forEach(this.addDependency)
           }.bind(this)
         }),
         require('cssnext')({
           url: false,
-          import: false,
-        }),
+          import: false
+        })
       ]
     },
     resolve: {
-      extensions: ['', '.js', '.jsx', '.sass', '.scss', '.less', '.css'],
+      extensions: ['', '.js', '.jsx', '.sass', '.scss', '.less', '.css']
     },
     plugins: options.production ? [
       // Important to keep React file size down
       new webpack.DefinePlugin({
-        "process.env": {
-          "NODE_ENV": JSON.stringify("production"),
-        },
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
       }),
       new webpack.optimize.DedupePlugin(),
       new webpack.optimize.UglifyJsPlugin({
         compress: {
-          warnings: false,
-        },
+          warnings: false
+        }
       }),
-      new ExtractTextPlugin("app.[hash].css"),
+      new ExtractTextPlugin('app.[hash].css'),
       new HtmlWebpackPlugin({
         template: './conf/tmpl.html',
-        production: true,
-      }),
+        production: true
+      })
     ] : [
       new HtmlWebpackPlugin({
-        template: './conf/tmpl.html',
-      }),
-    ],
-  };
-};
+        template: './conf/tmpl.html'
+      })
+    ]
+  }
+}
